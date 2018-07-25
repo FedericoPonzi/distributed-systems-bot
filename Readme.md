80cfaeff8aeadf5df4a1e6c23404031ea713caa4
Federico Ponzi <isaacisback92@gmail.com>
2018-07-25 18:03:16 +0200

    Initial commit.

---
+++ Readme.md
@@ -0,0 +1,21 @@
+## Distributed Systems bot
+@distribsystems
+
+This bot has the following aims:
+
+ 1. Tweet links from feeds of interesting websites
+ 1. Retweet and like "interesting" tweets
+ 3. Schedule posts based on a publication timetable (with exceptions - like "publish now"), made from a Telegram bot interface.
+ 4. Save every tweet in the database, to do some analysis - based on interactions.
+ 5. A web based interface to let users review the sources of news.
+ 6. Use a shortlink system while publishing tweets, to track clicks and understand 
+        which feeds are more interesting for the users.
+
+## 1. Feed RSS
+At the moment, the bot is mainly using the free service from iffft.com (thanks!).
+
+I'm working on an alternative which will better suits the needs of the bot.
+
+## 3. Telegram bot + post at
+Why telegram? Because I like the option to schedule a post by just sending a message on Telegram.
+
---
+++ repository.go
@@ -0,0 +1,96 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+	"time"
+)
+
+type MysqlRepository struct{
+	db *sql.DB
+}
+func NewMysqlRepository() (mysqlRepository * MysqlRepository){
+
+	return &MysqlRepository{connectDb()}
+}
+
+type FeedRss struct{
+	id int
+	twitterHandle string
+	url string
+	name string
+	category int
+}
+
+func connectDb() *sql.DB{
+	db, err := sql.Open("mysql", config.getDbConnectionString())
+	log.Println(config.getDbConnectionString())
+	if err != nil {
+		log.Fatal("Impossible open db: " , err)
+	}
+	err = db.Ping()
+	if err != nil {
+		log.Fatal("Problem with connection to the database:", err)
+	}
+	return db
+}
+
+func (repo *MysqlRepository) close(){
+	repo.db.Close()
+}
+
+
+func (repo *MysqlRepository) getAllFeedRss() (toRet []*FeedRss){
+	rows, err := repo.db.Query("select id, name, twitterHandle, url from feed_rss")
+	fatalIfErr(err)
+	defer rows.Close()
+
+	for rows.Next() {
+		feed := new(FeedRss)
+		err := rows.Scan(&feed.id, &feed.name, &feed.twitterHandle, &feed.url)
+		if err != nil {
+			log.Fatal(err)
+		}
+		toRet = append(toRet, feed)
+	}
+	err = rows.Err()
+	if err != nil {
+		log.Fatal(err)
+	}
+	return toRet
+}
+
+
+func (repo *MysqlRepository) findFeedRssIdByUrl(url string) (id int) {
+	log.Println("Looking for id, from url: ", url)
+	err := repo.db.QueryRow("select id from feed_rss where url = ?", url).Scan(&id)
+		if err != nil {
+		log.Fatal(err)
+	}
+	return id
+}
+func (repo *MysqlRepository) addFeedRssVisited(feed_id int, updated *time.Time) {
+	stmt, err := repo.db.Prepare("INSERT INTO feed_rss_visited(feed_id,updated) VALUES(?,?)")
+	if err != nil {
+		log.Fatal(err)
+	}
+	res, err := stmt.Exec(feed_id, updated)
+	if err != nil {
+		log.Fatal(err)
+	}
+	_, err = res.LastInsertId()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+func (repo *MysqlRepository) getLastFeedRssUpdatedByFeedId(id int) (t time.Time) {
+	log.Println("Looking for last update for visited: ", id)
+	err := repo.db.QueryRow("select updated from feed_rss_visited where feed_id = ? limit 1;", id).Scan(&t)
+	if err != nil {
+		log.Println("getLastFeedRssUpdatedByFeedId: error ", err)
+		t = time.Now() // Because probably this is a new feed rss source.
+	}
+
+	return t
+	//return time.Now().AddDate(0, -3, 0)
+}
