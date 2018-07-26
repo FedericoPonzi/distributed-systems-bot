777d89da9ffc10e29456a92ab65d4bbdac83d18d
Federico Ponzi <isaacisback92@gmail.com>
2018-07-26 20:26:31 +0200

    Adds feed-rss in the sql definition.

--- resources/distribsystems.sql
+++ resources/distribsystems.sql
@@ -1,3 +1,12 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.4.1deb2ubuntu2
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Jul 26, 2018 at 08:26 PM
+-- Server version: 5.7.22-0ubuntu0.16.04.1-log
+-- PHP Version: 7.0.30-0ubuntu0.16.04.1
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
@@ -22,6 +31,14 @@ CREATE TABLE `feed_category` (
   `name` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `feed_category`
+--
+
+INSERT INTO `feed_category` (`id`, `name`) VALUES
+(1, 'Personal Blog'),
+(2, 'Corporate News Feeds');
+
 -- --------------------------------------------------------
 
 --
@@ -35,6 +52,15 @@ CREATE TABLE `feed_rss` (
   `url` varchar(150) NOT NULL,
   `category_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `feed_rss`
+--
+
+INSERT INTO `feed_rss` (`id`, `name`, `twitterHandle`, `url`, `category_id`) VALUES
+(1, 'The Morning Paper', 'adriancolyer', 'https://blog.acolyer.org/feed/', 1),
+(2, 'GCP Data & ML', 'gcpdataml', 'https://cloud.google.com/blog/big-data/feed.xml', 2),
+(3, 'The Paper Trail', 'henryr', 'http://www.the-paper-trail.org/index.xml', 1);
 
 -- --------------------------------------------------------
 
